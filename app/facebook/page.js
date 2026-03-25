fe8d10de624ec207cfe614f4d1c170531181f7f3
fb12-thonk <fb1202598@gmail.com>
2026-03-25 14:25:28 +0700

    Update page.js

--- app/facebook/page.js
+++ app/facebook/page.js
@@ -3,12 +3,6 @@ import { useState } from 'react';
 import MainBox from '../components/MainBox';
 import ContentArea from '../components/ContentArea';
 
-// META TAG SEO GOOGLE
-export const metadata = {
-  title: `Facebook Video Downloader HD & 4K - ${siteConfig.SITENAME}`,
-  description: `Download Facebook videos in Full HD and 4K quality for free. Fast, secure, and works for FB Reels and private videos on ${siteConfig.SITENAME}.`,
-  keywords: `Facebook downloader, download FB videos, Facebook video saver, FB reels downloader, Facebook to MP4, download private FB videos, ${siteConfig.SITENAME}`,
-};
 
 export default function FacebookDownloader() {
   const [url, setUrl] = useState('');
