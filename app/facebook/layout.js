f864cb24f22e02d8b2ba4115254c30c49f2c568e
fb12-thonk <fb1202598@gmail.com>
2026-03-25 14:41:56 +0700

    Create layout.js

---
+++ app/facebook/layout.js
@@ -0,0 +1,11 @@
+import { siteConfig } from '../config';
+
+export const metadata = {
+  title: `Facebook Video Downloader HD & 4K - ${siteConfig.SITENAME}`,
+  description: `Download Facebook videos in Full HD and 4K quality for free. Fast, secure, and works for FB Reels and private videos on ${siteConfig.SITENAME}.`,
+  keywords: `Facebook downloader, download FB videos, Facebook video saver, FB reels downloader, Facebook to MP4, download private FB videos, ${siteConfig.SITENAME}`,
+};
+
+export default function Layout({ children }) {
+  return <>{children}</>;
+}
